30a4083dd447d75d5fddcca6641ad63ea5751a03
fernan9 <fernan954@gmail.com>
2016-04-29 18:51:41 -0500

    Update codonCaller.py

--- codonCaller.py
+++ codonCaller.py
@@ -47,6 +47,7 @@ import subprocess
 import pandas as pd
 from StringIO import StringIO
 from subprocess import check_output
+import numpy as np
 
 def run(cmd, logfile):
     p = subprocess.Popen(cmd, shell=True, universal_newlines=True, stdout=logfile)
@@ -54,13 +55,9 @@ def run(cmd, logfile):
     logfile.flush()
     return ret_code
 
-
-
 # 1. First we read all the files in local directory with the extension .EXOME
 exomes = glob.glob('*.exome')
 paths = glob.glob('*/')
-
-#print type(exomes)
 
 # 2. Second for each file (describing one gene by exons)
 for exome in exomes:
@@ -69,34 +66,37 @@ for exome in exomes:
 #	A. load the file as a DataFrame with pandas
     exome_df = pd.read_table(exome, header = None)
 #	B. Read each row to apply
+
+###   probably we need to define here the df for the complete gene
+
     for exon in exome_df.itertuples():
 #		a. drive a masking of the related SCAF file
 # 			$ awk '{ if ($2 >= limites(433041) && $2 <= limites(433055)) print }' DPSCF300328.scaf
         scaf = exon[1] + '.scaf'
         limit = (exon[4],exon[5])
         comando = "awk '{{ if ($2 >= {0} && $2 <= {1}) print }}' {2}".format(min(limit),max(limit), scaf)
-#        comando = ["awk", "'{{ if ($2 >= {0} && $2 <= {1}) print }}'".format(min(limit),max(limit)), "{0}".format(scaf)]
-        print limit
+#       next evaluation catches exceptions to avoid crash
         try:
             salida = subprocess.check_output(comando, stderr=subprocess.STDOUT, shell = True)
         except subprocess.CalledProcessError as e:
             raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
-
-"""
-#		b. create a DataFrame of it
+        if salida == "": #this line catches an empty return to avoid crash
+            continue
+#		b. create a DataFrame of it      
         temp_df = pd.read_table(StringIO(salida), header = None)
+        tamano = len(temp_df.index)
 #		c. repeat data from each Exon Row and merge it to the masked Scaffold
-        d = {'GENID' : np.repeat(geneID, len(df)),
-            'INIEX' : np.repeat(exon(4), len(df)),
-            'ENDEX' : np.repeat(exon(5), len(df)),
-            'STRND' : np.repeat(exon(7), len(df)),
-            'PHASE' : np.repeat(exon(8), len(df))}
-        var_df = pd.DataFrame(d)
+        var_df = pd.DataFrame({'01genid' : np.repeat(geneID, tamano),
+                               '02iniex' : np.repeat(exon[4], tamano),
+                               '03endex' : np.repeat(exon[5], tamano),
+                               '04strnd' : np.repeat(exon[7], tamano),
+                               '04frame' : np.repeat(exon[8], tamano)})
 #		d. paste data to an output file with the GeneID.VAR
         result = pd.concat([var_df, temp_df], axis=1)
-        df.to_csv(geneID+'.var', encoding = 'utf-8')
+#       this side needs to concatenate column-wise before printing each exon
+        result.to_csv(geneID+'.var',mode = 'a', encoding = 'utf-8')
         print result.head(10)
 #3. Assign number codon position of subtitutions
-
+"""
 #4. Ka/Ks ratio
 """
